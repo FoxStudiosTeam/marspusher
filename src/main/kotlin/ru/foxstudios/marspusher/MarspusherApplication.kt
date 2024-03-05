4241e1a145ce36900f36e15c6583096730a9b32a
AgniaEndie <Endienasg3@gmail.com>
2024-03-06 02:54:45 +0500

    fix Main.kt

--- src/main/kotlin/ru/foxstudios/marspusher/MarspusherApplication.kt
+++ src/main/kotlin/ru/foxstudios/marspusher/MarspusherApplication.kt
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @Slf4j
