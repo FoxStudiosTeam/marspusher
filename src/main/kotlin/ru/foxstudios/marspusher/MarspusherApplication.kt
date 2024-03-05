bc5cd8e9ed7355e3b44eae25021749eb48f688ab
AgniaEndie <Endienasg3@gmail.com>
2024-03-06 02:47:35 +0500

    update Main.kt

--- src/main/kotlin/ru/foxstudios/marspusher/MarspusherApplication.kt
+++ src/main/kotlin/ru/foxstudios/marspusher/MarspusherApplication.kt
@@ -1,12 +1,18 @@
 package ru.foxstudios.marspusher
 
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-class MarspusherApplication
+@Slf4j
+class MarspusherApplication {
 
-fun main(args: Array<String>) {
-    runApplication<MarspusherApplication>(*args)
+    fun main(args: Array<String>) {
+        val logger = LoggerFactory.getLogger(this::class.java)
+        logger.info("WORK!!!!!!")
+        runApplication<MarspusherApplication>(*args)
+    }
 }
