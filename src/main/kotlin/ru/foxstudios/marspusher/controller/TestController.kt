dec6d3ace5449fe8e6470bf8c37db3a875f0c380
AgniaEndie <Endienasg3@gmail.com>
2024-03-06 02:43:36 +0500

    fix TestController.kt for k8s compability

--- src/main/kotlin/ru/foxstudios/marspusher/controller/TestController.kt
+++ src/main/kotlin/ru/foxstudios/marspusher/controller/TestController.kt
@@ -1,9 +1,11 @@
 package ru.foxstudios.marspusher.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/clown")
 class TestController {
     @GetMapping("/test")
     fun test() : String{
