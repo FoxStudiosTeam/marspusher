9d6ef54154f598a86146740a3357e1fe56e53aeb
AgniaEndie <Endienasg3@gmail.com>
2024-03-06 02:09:55 +0500

    fix TestController.kt

--- src/main/kotlin/ru/foxstudios/marspusher/controller/TestController.kt
+++ src/main/kotlin/ru/foxstudios/marspusher/controller/TestController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
-    @GetMapping("test")
+    @GetMapping("/test")
     fun test() : String{
         return "work!!"
     }
