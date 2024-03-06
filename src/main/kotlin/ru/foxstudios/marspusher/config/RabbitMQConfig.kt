6ba85c448fa2e44fa7f61552d4509ae0abe1aae6
AgniaEndie <Endienasg3@gmail.com>
2024-03-06 16:01:06 +0500

    update MessageBrokerConfig

--- src/main/kotlin/ru/foxstudios/marspusher/config/RabbitMQConfig.kt
+++ src/main/kotlin/ru/foxstudios/marspusher/config/RabbitMQConfig.kt
@@ -1,13 +1,36 @@
 package ru.foxstudios.marspusher.config
 
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+
 @Configuration
-class RabbitMQConfig {
+class RabbitMQConfig(@Value("marspusher.rabbitmq.login") var password: String, @Value("marspusher.rabbitmq.password") var login: String) {
     @Bean
-    fun messageQueue() : Queue{
+    fun messageQueue(): Queue {
         return Queue("mars-queue")
     }
+
+    @Bean
+    fun connectionFactory(): ConnectionFactory {
+        val cachingConnectionFactory = CachingConnectionFactory("mars-listener-service")
+        cachingConnectionFactory.port = 15672
+        cachingConnectionFactory.username = login
+        cachingConnectionFactory.setPassword(password)
+        cachingConnectionFactory.virtualHost = "cpp"
+        return cachingConnectionFactory
+    }
+
+    @Bean
+    fun rabbitTemplate() : RabbitTemplate{
+        return RabbitTemplate(connectionFactory())
+    }
+
+
+
 }
